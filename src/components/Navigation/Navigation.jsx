c89789af0ae61d20fd945f62ec612774f4a8afdc
Tanushree Paul <125464289+tanu0604@users.noreply.github.com>
2025-07-01 17:30:09 +0530

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
 import { Link } from "react-router-dom";
-
+import navLogo from "../../assets/navImg.png";
 const navItems = [
   { name: "Home", href: "/" },
   { name: "About", href: "/about" },
   { name: "Price Calculator", href: "/calculator" },
   { name: "Form", href: "/form" },
   { name: "Gallery", href: "/gallery" },
-
 ];
 
 export default function Navigation() {
@@ -18,9 +17,12 @@ export default function Navigation() {
     <nav className="fixed top-0 w-full bg-black/70 backdrop-blur-md z-50 shadow-lg">
       <div className="max-w-7xl mx-auto px-6 lg:px-10 flex justify-between items-center h-16">
         {/* Logo */}
-        <Link to="/" className="text-2xl font-bold text-white tracking-wide">
-          LLuxe<span className="text-gray-300">Living</span>
-        </Link>
+        <div className="flex items-center space-x-4">
+          <img src={navLogo} alt="LLuxe Living" className="w-24 h-fit" />
+          <Link to="/" className="text-2xl font-bold text-white tracking-wide">
+            LLuxe<span className="text-gray-300">Living</span>
+          </Link>
+        </div>
 
         {/* Desktop Navigation */}
         <div className="hidden md:flex space-x-8">
